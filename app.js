03d06763f2691a303ca89abbad97af979d6541e8
Dimassz <78773792+Dimassz@users.noreply.github.com>
2023-10-23 01:36:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(
     session({
       secret: 'my_secret_key',
