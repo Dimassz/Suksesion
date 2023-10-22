91fabe52008b720e3c534d06842c8a93b2481bae
Dimassz <78773792+Dimassz@users.noreply.github.com>
2023-10-23 01:57:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const port = process.env.PORT || 3000;
 const path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + "/public/"));
 app.use(express.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
